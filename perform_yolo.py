06321255b4f46ce21d3de0fb15524d308bbf0f93
Franklin Xavier <franklinxavier01@gmail.com>
2024-04-09 22:52:54 -0500

    Added Cuda

--- perform_yolo.py
+++ perform_yolo.py
@@ -1,6 +1,7 @@
 # Import Necessary Libraries
 from ultralytics import YOLO
 import numpy as np
+import torch
 
 
 # Define a Function to Perform YOLO on Images
@@ -8,6 +9,8 @@ def PerformYolo(left_image, right_image):
 
     # Load the YOLOv8 Pretrained Model
     model = YOLO('yolov8n.pt')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.to(device)
 
     # Predict the Objects in Left and Right Images using Model
     detections_left_image = model(left_image, verbose = False)
