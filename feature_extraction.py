32cccbcb39a545b480f087ddd9ef8b24b2bea76e
Tzu-Hsien(Ben) Lee <lee04484@umn.edu>
2024-04-13 21:04:02 -0500

    add FeaturePoint class, and update feature_extraction.py accordingly

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,7 +1,7 @@
 # Import Necessary Libraries
 import numpy as np
 import cv2
-
+from FeaturePoint import FeaturePoint
 
 # Define a Function to Extract Feature Points
 def FeatureExtraction(left_img, right_img):
@@ -31,8 +31,7 @@ def FeatureExtraction(left_img, right_img):
     matches = sorted(matches, key = lambda x: x.distance)
 
     # Compute Feature Points and Disparity for all Matches
-    feature_points = []   
-    disparity = []
+    feature_points = []
 
     # For all Matches
     for m in matches:
@@ -42,12 +41,71 @@ def FeatureExtraction(left_img, right_img):
         right_index = m.trainIdx
 
         # Compute Feature Points and Disparity
-        feature_points.append([left_keypoints[left_index].pt[0], left_keypoints[left_index].pt[1], right_keypoints[right_index].pt[0], right_keypoints[right_index].pt[1]])
-        disparity.append([left_keypoints[left_index].pt[0] - right_keypoints[right_index].pt[0]])
+        if (abs(left_keypoints[left_index].pt[1] - right_keypoints[right_index].pt[1]) > 0.001):
+            continue
+        
+        feature_point = FeaturePoint()
+        
+        feature_point.left_pt = (left_keypoints[left_index].pt[0], left_keypoints[left_index].pt[1])
+        feature_point.left_descriptor = left_desc[left_index]
+        
+        feature_point.right_pt = (right_keypoints[right_index].pt[0], right_keypoints[right_index].pt[1])
+        feature_point.right_descriptor = right_desc[right_index]
+
+        feature_point.disparity = left_keypoints[left_index].pt[0] - right_keypoints[right_index].pt[0]
+        feature_points.append(feature_point)
 
     # Convert into Numpy Array
-    feature_points = np.array(feature_points)
-    disparity = np.array(disparity)
+    # feature_points = np.array(feature_points)
 
     # Return Feature Points and Disparity
-    return feature_points, disparity
+    return feature_points
+
+# Old version
+# def FeatureExtraction(left_img, right_img):
+
+#     # Create an ORB Object to Extract Keypoints
+#     orb = cv2.ORB_create() 
+
+#     # Find the Keypoints of Left and Right Images
+#     left_keypoints, left_desc = orb.detectAndCompute(left_img, None) 
+#     right_keypoints, right_desc = orb.detectAndCompute(right_img, None)
+
+#     # Define Parameters for Matching Keypoints
+#     FLANN_INDEX_LSH = 6
+
+#     index_params = dict(algorithm = FLANN_INDEX_LSH,
+#                         table_number = 6,  
+#                         key_size = 12,   
+#                         multi_probe_level = 1)  
+    
+#     search_params = dict(checks = 50)  
+    
+#     # Create a Flann Based Matcher
+#     flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+#     # Find Matches and Sort based On Distances
+#     matches = flann.match(left_desc, right_desc)
+#     matches = sorted(matches, key = lambda x: x.distance)
+
+#     # Compute Feature Points and Disparity for all Matches
+#     feature_points = []   
+#     disparity = []
+
+#     # For all Matches
+#     for m in matches:
+
+#         # Get the Indices of Matches
+#         left_index = m.queryIdx
+#         right_index = m.trainIdx
+
+#         # Compute Feature Points and Disparity
+#         feature_points.append([left_keypoints[left_index].pt[0], left_keypoints[left_index].pt[1], right_keypoints[right_index].pt[0], right_keypoints[right_index].pt[1]])
+#         disparity.append([left_keypoints[left_index].pt[0] - right_keypoints[right_index].pt[0]])
+
+#     # Convert into Numpy Array
+#     feature_points = np.array(feature_points)
+#     disparity = np.array(disparity)
+
+#     # Return Feature Points and Disparity
+#     return feature_points, disparity
---
+++ test_depth.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+import matplotlib.pyplot as plt
+from feature_extraction import FeatureExtraction
+
+if __name__ == "__main__":
+    left_images_folder = 'Dataset_1/Left_Images/'
+    right_images_folder = 'Dataset_1/Right_Images/'
+
+    left_images = os.listdir(left_images_folder)
+    right_images = os.listdir(right_images_folder)
+
+    left_images = [os.path.abspath(left_images_folder + '/' + left_image) for left_image in left_images]
+    right_images = [os.path.abspath(right_images_folder + '/' + right_image) for right_image in right_images]
+
+    left_image = cv2.imread(left_images[0])
+    right_image = cv2.imread(right_images[0])
+
+    feature_points = FeatureExtraction(left_image, right_image)
+
+    fp_sorted = sorted(feature_points, key=lambda x: x.disparity)
+
+    for fp in fp_sorted:
+        lu = int(fp.left_pt[0])
+        lv = int(fp.left_pt[1])
+        cv2.circle(left_image, (lu, lv), radius=5, color=(255, 0, 0), thickness=-1)
+
+        ru = int(fp.right_pt[0])
+        rv = int(fp.right_pt[1])
+        cv2.circle(right_image, (ru, rv), radius=5, color=(255, 0, 0), thickness=-1)
+
+        fig, axes = plt.subplots(1, 2, figsize=(100, 50))
+        axes[0].imshow(left_image)
+        axes[0].set_title('Left 1')
+        axes[1].imshow(right_image)
+        axes[1].set_title('Right 2')
+        plt.tight_layout()
+        plt.text(0, 0, f'({fp.disparity})')
+        plt.show()
+        cv2.circle(left_image, (lu, lv), radius=5, color=(0, 0, 255), thickness=-1)
+        cv2.circle(right_image, (ru, rv), radius=5, color=(0, 0, 255), thickness=-1)
+    print("-----")
