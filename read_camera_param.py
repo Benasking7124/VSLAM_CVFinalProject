4d293b20a5ec7b096c8efad0e665d6e3057d1d8a
Tzu-Hsien(Ben) Lee <lee04484@umn.edu>
2024-04-21 02:11:56 -0500

    Misspelling in comment

--- read_camera_param.py
+++ read_camera_param.py
@@ -27,7 +27,7 @@ def read_camera_param(filepath):
 
     @param {str} filepath - path of calib.txt
 
-    @return {dict} dictionary containing projection matrix, focal length, and baseline
+    @return {dict} dictionary containing projection matrices, focal length, and baseline
     """
 
     # Create Camera Parameter Dictionary
@@ -40,7 +40,7 @@ def read_camera_param(filepath):
     P_rect_00 = np.reshape(calib_data['P0'], (3, 4))
     P_rect_10 = np.reshape(calib_data['P1'], (3, 4))
 
-    # Extract Projection Matrixes, Focal Length, and Baselie
+    # Extract Projection Matrices, Focal Length, and Baselie
     camera_param['left_projection'] = P_rect_00
     camera_param['right_projection'] = P_rect_10
     camera_param['focal_length'] = P_rect_00[0][0]
