939997e0adf7f42beae4c4731bdc6acd9b5170fd
Siddharth <siddharthnathan@gmail.com>
2024-04-24 22:53:43 -0500

    Update

--- compute_reproj_error.py
+++ compute_reproj_error.py
@@ -25,8 +25,8 @@ def ComputeReprojError(feature_points, camera_param):
 
     Args:
         feature_points (numpy.ndarray): 2D feature point in current image.
-        T_c_w_j (numpy.ndarray): Transform from world to camera frame j.
-        T_c_w_i (numpy.ndarray): Transform from world to camera frame i.
+        T_c_w_j (numpy.ndarray): Transform from world to next camera frame
+        T_c_w_i (numpy.ndarray): Transform from world to current camera frame
         P_n_w (numpy.ndarray): 3D point in world coordinates.
         z_n_c_i (float): Depth of the 3D point in camera frame i.
         camera_param (numpy.ndarray): Camera intrinsic matrix.
@@ -37,9 +37,13 @@ def ComputeReprojError(feature_points, camera_param):
 
     # Initialise Vectors
     image_coordinates = np.empty([0, 3])
+    T_c_w_i = np.identity(3)
 
     # For every Feature Point
     for ind in range(feature_points.num_fp):
 
         # Compute 3D Camera Coordinates from 2D Image Coordinates
-        image_coordinates = np.vstack([image_coordinates, [inverse_project_points(feature_points.left_pts[ind], feature_points.depth[ind], camera_param['left_projection'])]])
+        current_image_coordinates = np.vstack([image_coordinates, [inverse_project_points(feature_points.left_pts[ind], feature_points.depth[ind], camera_param['left_projection'])]])
+
+        # Transform 3D Camera Coordinates from Current Frame to World Frame
+        current_world_coordinates = (T_c_w_i @ current_image_coordinates.T).T
